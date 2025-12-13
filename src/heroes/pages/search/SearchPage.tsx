932ac9d59afd3e4d683befad0ecbcbcd7346640b
Selene Posadas <seleneposadass@gmail.com>
2025-12-13 03:08:29 -0300

    Refactorizacion de la pagina de inicio y de busqueda de personajes.

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,8 +1,32 @@
+import { CustomJumbotron } from '@/components/custom/CustomJumbotron';
+import { HeroStats } from '@/heroes/components/HeroStats';
+import { SearchControls } from './ui/SearchControls';
+import { CustomBreadcrumb } from '@/components/custom/CustomBreadCrumb';
+
 export const SearchPage = () => {
   return (
-    <div>SearchPage</div>
-  )
-};
+    <>
+      <CustomJumbotron
+        title="Búsqueda de SuperHéroes"
+        description="Descubre, explora y administra super héroes y villanos"
+      />
 
+      <CustomBreadcrumb
+        currentPage="Buscador de héroes"
+        // breadcrumbs={[
+        //   { label: 'Home1', to: '/' },
+        //   { label: 'Home2', to: '/' },
+        //   { label: 'Home3', to: '/' },
+        // ]}
+      />
+
+      {/* Stats Dashboard */}
+      <HeroStats />
+
+      {/* Filter and search */}
+      <SearchControls />
+    </>
+  );
+};
 
 export default SearchPage;
